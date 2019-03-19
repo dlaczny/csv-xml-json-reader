321f076e008f2e72278b6192bd52ae75debafcd9
dawid łaczny <lacznypl@hotmail.com>
2019-03-19 09:13:00 +0100

    Add project files.

---
+++ csv-xml-json-reader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csv_xml_json_reader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
